9347f79a45043fd291ad06bc6e9fe01edecb4260
yossibank <yossibank@yahoo.co.jp>
2021-03-06 21:16:29 +0900

    CocoaPods導入:フレームワーク(RSwift, KeychainAccess, Nuke)

---
+++ Pods/Target Support Files/Pods-AddtionalTraining-iOS-AddtionalTraining-iOSUITests/Pods-AddtionalTraining-iOS-AddtionalTraining-iOSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AddtionalTraining_iOS_AddtionalTraining_iOSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AddtionalTraining_iOS_AddtionalTraining_iOSUITests
+@end
