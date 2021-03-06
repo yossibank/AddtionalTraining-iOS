ae09f308669ba7b289d6bbe9fbaa13307932d23f
yossibank <yossibank@yahoo.co.jp>
2021-03-06 21:19:28 +0900

    gitignore設定

--- Pods/Target Support Files/Pods-AddtionalTraining-iOS-AddtionalTraining-iOSUITests/Pods-AddtionalTraining-iOS-AddtionalTraining-iOSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AddtionalTraining_iOS_AddtionalTraining_iOSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AddtionalTraining_iOS_AddtionalTraining_iOSUITests
-@end
--- Pods/Target Support Files/Pods-AddtionalTraining-iOS/Pods-AddtionalTraining-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AddtionalTraining_iOS : NSObject
-@end
-@implementation PodsDummy_Pods_AddtionalTraining_iOS
-@end
