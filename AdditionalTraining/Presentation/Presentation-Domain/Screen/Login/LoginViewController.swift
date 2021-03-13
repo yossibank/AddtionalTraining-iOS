d8f6419a40c9a3ad5d351ee771970aa7fab55bcd
yossibank <yossibank@yahoo.co.jp>
2021-03-13 13:05:33 +0900

    サインアップ、ログインベース画面作成

---
+++ AdditionalTraining/Presentation/Presentation-Domain/Screen/Login/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  AdditionalTraining
+//
+//  Created by KAMIYAMA YOSHIHITO on 2021/03/13.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ AdditionalTraining/Presentation/Presentation-Domain/Screen/Signup/SignupViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SignupViewController.swift
+//  AdditionalTraining
+//
+//  Created by KAMIYAMA YOSHIHITO on 2021/03/13.
+//
+
+import UIKit
+
+final class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
